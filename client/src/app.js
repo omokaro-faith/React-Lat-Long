539748e70c2f7fb4a79826c5a4a5c16f89c08c80
andela-fomokaro <faith.omokaro@andela.com>
2018-12-28 20:29:26 +0100

    add react-router-dom for client side rendering, add routes

--- client/src/app.js
+++ client/src/app.js
@@ -1,5 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import '../styles/styles.scss';
 
-ReactDOM.render(<p>This is my boiler plate</p>, document.getElementById('app'));
+const Login = () => (
+  <p>This is the Login page</p>
+)
+
+const Home = () => (
+  <p>This is the Home page</p>
+)
+
+const Profile = () => (
+  <p>This is your Profile page</p>
+)
+
+const NotFoundPage = () => (
+  <div>404!</div>
+)
+const route = (
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={Home} exact={true}/>
+      <Route path="/login" component={Login} />
+      <Route path="/profile" component={Profile}/>
+      <Route component={NotFoundPage}/>
+    </Switch>
+  </BrowserRouter>
+)
+
+ReactDOM.render(route, document.getElementById('app'));
