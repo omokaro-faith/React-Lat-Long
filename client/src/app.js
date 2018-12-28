0490d0e8436f89f61516d7f958d091f3288ed8f7
andela-fomokaro <faith.omokaro@andela.com>
2018-12-28 20:55:25 +0100

    add nav, pagenotfound and footer component

--- client/src/app.js
+++ client/src/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
 import '../styles/styles.scss';
 
 const Login = () => (
@@ -16,16 +16,40 @@ const Profile = () => (
 )
 
 const NotFoundPage = () => (
-  <div>404!</div>
+  <div>
+    <h2>Page Not Found</h2>
+    <h3>We couldn't find what you were looking for.</h3>
+  </div>
 )
+
+const Header = () => (
+  <header>
+    <h1>Lat-Lang</h1>
+    <NavLink to="/" activeClassName="is-active" exact={true}> Home </NavLink>
+    <NavLink to="/login" activeClassName="is-active" exact={true}> Login </NavLink>
+    <NavLink to="/profile" activeClassName="is-active" exact={true}> Profile </NavLink>
+  </header>
+)
+
+const Footer = () => (
+  <footer>
+    <p>This is the footer</p>
+  </footer>
+)
+
+
 const route = (
   <BrowserRouter>
+  <div>
+    <Header />
     <Switch>
       <Route path="/" component={Home} exact={true}/>
       <Route path="/login" component={Login} />
       <Route path="/profile" component={Profile}/>
       <Route component={NotFoundPage}/>
     </Switch>
+    <Footer />
+  </div>
   </BrowserRouter>
 )
 
